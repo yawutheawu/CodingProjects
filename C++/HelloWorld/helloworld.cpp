1da57abeeb7bce90946d651440f0a881607c21f7
yawutheawu <mayklprime@gmail.com>
2024-07-06 10:08:44 -0400

    C++ Hello World files

--- C++/HelloWorld/helloworld.cpp
+++ C++/HelloWorld/helloworld.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//https://www.w3schools.com/cpp/cpp_getstarted.asp
 int main() {
   cout << "Hello World!";
   return 0;
---
+++ C++/HelloWorldNoStandardLib/HelloWorld2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+//https://www.w3schools.com/cpp/cpp_syntax.asp
+int main() {
+  std::cout << "Hello World!";
+  return 0;
+}
