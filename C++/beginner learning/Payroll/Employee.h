d2984b442ffcd5372757a239e421111bd3ae1d3e
yawutheawuAWAY <150863581+nate-24@users.noreply.github.com>
2024-11-16 12:01:31 -0500

    This commit was pushed automatically to CodingProjects (Not the best idea)

---
+++ C++/beginner learning/Payroll/Employee.h
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Employee {
+    private:
+        std::string FirstName;
+        std::string LastName;
+        std::string EmployeeID = "000000000";
+        std::string Title;
+        double compensation;
+        bool Wagie = false;
+        double unusedVacation = 0;
+        double hoursWorked = 0;
+        double paidSickTime = 0;
+    public:
+        Employee(){}
+        Employee(std::string ID,std::string NombreUno, std::string NombreDos, std::string Rank, double Payment, bool Waged = false){
+            FirstName = NombreUno;
+            LastName = NombreDos;
+            Title = Rank;
+            EmployeeID = ID;
+            compensation = Payment;
+            Wagie = Waged;
+        }
+        std::string getName() {
+            return FirstName + " " + LastName;
+        }
+        std::string getID() {
+            return EmployeeID;
+        }
+        bool getWaged() {
+            return Wagie;
+        }
+        double getComp() {
+            return compensation;
+        }
+        double getUnsedVacation() {
+            return unusedVacation;
+        }
+        double getHours() {
+            return hoursWorked;
+        }
+        double getPaidSickTime() {
+            return paidSickTime;
+        }
+        void updateTimeWorked(double newHours) {
+            hoursWorked = newHours;
+        }
+        void updateUnusedVacation(double newVacay) {
+            unusedVacation = newVacay;
+        }
+        void updateSickLeave(double newSickLeave) {
+            paidSickTime = newSickLeave;
+        }
+
+};
---
+++ C++/beginner learning/Payroll/PayRollMain.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Employee.h"
+
+int main() {
+    Employee numba1 = Employee();
+    std::cout << numba1.getName();
+    return 0;
+}
