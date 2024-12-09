74b6fd3420fe05f3e8dcf2b000330d7727db4a70
yawutheawu <mayklprime@gmail.com>
2024-12-09 17:42:43 -0500

    Automatic Push of CodingProjects/

---
+++ Fun Bash and Batch/Run Streamlit File/Run Website (Windows).bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+:: Runs Website
+TITLE Website Runner
+ECHO Running Website
+streamlit run "/Users/yawu/Desktop/Repos/BentleyCSworkSoph/CS230 (intro python)/Projects/Final/Home.py"
+ECHO Done
+TIMEOUT 2
