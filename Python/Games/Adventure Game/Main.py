e3bb906d370b1fd534e242ad24860a397184ff01
yawutheawuAWAY <150863581+nate-24@users.noreply.github.com>
2025-04-12 13:37:44 -0400

    This commit was pushed automatically to CodingProjects (Not the best idea)

--- Python/Games/Adventure Game/Main.py
+++ Python/Games/Adventure Game/Main.py
@@ -2,8 +2,8 @@ def askQuestion(prompt:str, question:str, choices:list) -> int:
     x = ""
     while not x in range(1,len(choices)+1):
         print(prompt)
-        for Val,Key in enumerate(choices):
-            print(f"\t{Val+1}. {Key}")
+        for Key,Val in enumerate(choices):
+            print(f"\t{Key+1}. {Val}")
         x = input(question)
         try:
             x = int(x)
@@ -13,7 +13,7 @@ def askQuestion(prompt:str, question:str, choices:list) -> int:
     return x-1
 
 def victory():
-    print("YOU WIN YIPPIIEEE")
+    print("We are so back")
     return 1
 
 def loss():
@@ -46,7 +46,67 @@ if output == 0:
         loss()
     elif output == 1:
         print("People smile eerily at you")
-        pass
+        prompt = "Will you eat a burger or drink apple juice?: "
+        question = "Choose a number: "
+        answerList = ["Eat a Burger", 
+                    "Drink Apple Juice"
+                    ]
+        output = askQuestion(prompt, question, answerList)
+        if output == 0:
+            prompt = "What kind of burger do you desire?: "
+            question = "Choose a number: "
+            answerList = ["CheeseBurger",
+                            "Double Whopper", 
+                            "Whopper"
+                            ]
+            output = askQuestion(prompt, question, answerList)
+            if output == 0:
+                print("A golden ticket to willy wonka's factory gets stuck between your teeth")
+                victory()
+            elif output == 1:
+                prompt = "What filling do you desire?: "
+                question = "Choose a number: "
+                answerList = ["Tuna",
+                            "Metal Pipes"
+                            ]
+                output = askQuestion(prompt, question, answerList)
+                if output == 0:
+                    print("The big fish bouncer takes offense to your frankly awful taste in burgers")
+                    loss()
+                else:
+                    print("The lead in the pipes gets to you")
+                    loss()
+            else:
+                prompt = "How much money do you wish to gamble for your whopper to win?: "
+                question = "Choose a number: "
+                answerList = ["all in",
+                              "None",
+                              "50%",
+                              "60%",
+                              "-30%"
+                            ]
+                output = askQuestion(prompt, question, answerList)
+                if output == 0:
+                    print("You win big")
+                    victory()
+                elif output == 1:
+                    print("It was all a dream (You get nothing)")
+                    loss()
+                elif output == 2:
+                    print("Your whopper disintegrates")
+                    loss()
+                elif output == 3:
+                    print("Your whopper leaves you and bets his own money")
+                    loss()
+                else:
+                    print("Your whopper can believe you have so little self-esteem and breaks up with you")
+                    loss()
+        else:
+            print("A person asks if you are drinking beer")
+            print("You decide to like and say yes")
+            print('They try your "beer"')
+            print("You die of humiliation")
+            loss()
     elif output == 2:
         print("You bet on green until you have no green")
         loss()
