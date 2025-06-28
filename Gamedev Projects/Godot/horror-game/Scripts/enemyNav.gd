5c8aa06a18065d893058d2360254da6e792906b3
yawutheawu <mayklprime@gmail.com>
2025-06-28 18:24:50 -0400

    This commit was pushed automatically to CodingProjects (Not the best idea)

--- Gamedev Projects/Godot/horror-game/Scripts/enemyNav.gd
+++ Gamedev Projects/Godot/horror-game/Scripts/enemyNav.gd
@@ -1,14 +1,14 @@
 extends CharacterBody3D
 
-@onready var player: CharacterBody3D = $Player
+@onready var player: CharacterBody3D = $"../Player"
 @onready var NavAgent = $NavigationAgent3D
 var SPEED = 0.5
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	NavAgent.set_target_position(player.position)
 	var destination = NavAgent.get_next_path_position()
 	var local_destination = destination - global_position
 	var direction = local_destination.normalized()
 	
-	velocity = direction * (SPEED *delta)
+	velocity = direction * SPEED
 	move_and_slide()
