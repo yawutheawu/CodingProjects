52b6e8d86f22757d3ebef191c56c32e07b7fa074
yawutheawuAWAY <150863581+nate-24@users.noreply.github.com>
2025-07-19 10:38:44 -0400

    This commit was pushed automatically to CodingProjects (Not the best idea)

--- Python/Sierpinski Carpet/Turtle/Sierpinski's Triangle.py
+++ Python/Sierpinski Carpet/Turtle/Sierpinski's Triangle.py
@@ -10,15 +10,31 @@ t.screen.title('Triangle maxxing')
 t.screen.colormode(255)
 xSize = 300
 ySize = 300
-resolution = 30
-TriSize = 500
+resolution = 50
+TriSize = 750
 t.screen.screensize(xSize*2,ySize*2)
+
+def getColor(x,y):
+    # -TriSize/2 to TriSize/2
+    # -(((3**(1/2)) / 2) * TriSize)/2 to (((3**(1/2)) / 2) * TriSize)/2
+    xScale = min(max(abs(x)/(TriSize/2),0),1)
+    yScale = min(max(abs(y)/((((3**(1/2)) / 2) * TriSize)/2),0),1)
+    r = 255 - int(min(max(255 * xScale,0),100))
+    b = int(min(max(255 * yScale,100),255))
+    g = 255 - int(min(max(255 * ((r+b)/(255*2)),55),150))
+    print(r,g,b, f"{(r+g+b)/(255*3)}")
+    return (r,g,b)
 
 def draw_sierpinski(length):
     if length<resolution:
+        t.color(getColor(t.pos()[0], t.pos()[1]))
+        t.begin_fill()
+        t.down()
         for i in range(0,3):
             t.fd(length)
             t.left(120)
+        t.end_fill()
+        t.up()
     else:
         draw_sierpinski(length/2)
         t.fd(length/2)
