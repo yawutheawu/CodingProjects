f43e3c1f09db975b4008be9692a8e62f1f776393
yawutheawuAWAY <150863581+nate-24@users.noreply.github.com>
2026-01-24 09:38:27 -0500

    This commit was pushed automatically to CodingProjects (Not the best idea)

--- Python/teaching/lambdas.py
+++ Python/teaching/lambdas.py
@@ -6,3 +6,5 @@ def x():
 x()
 
 (lambda x: print("I am lambda with no name, with value", x))(rand.randrange(1, 100))
+
+sortList = [["John", 25], ["Alice", 30], ["Bob", 20], ["Diana", 35], ["Charlie", 28], ["Eve", 22], ["Frank", 27], ["Grace", 24], ["Hank", 29], ["Ivy", 23]]
