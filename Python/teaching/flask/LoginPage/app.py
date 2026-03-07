d2f8b477a202e994512b603e4de02e95e990bf70
yawutheawuAWAY <150863581+nate-24@users.noreply.github.com>
2026-03-07 14:01:51 -0500

    This commit was pushed automatically to CodingProjects (Not the best idea)

--- Python/teaching/flask/LoginPage/app.py
+++ Python/teaching/flask/LoginPage/app.py
@@ -1,6 +1,20 @@
 from flask import Flask, request, render_template,abort, redirect, url_for
 from markupsafe import Markup
 import sqlite3 as sql
+import os
+
+def resetDir():
+    fileName = __file__
+    if type(fileName.split("\\")) == list and len(fileName.split("\\"))>1:
+        fileName = fileName.split("\\")[-1]
+        filePath = __file__.replace(fileName,"")
+    else:
+        fileName = fileName.split("/")[-1]
+        filePath = __file__.replace(fileName,"")
+    os.chdir(filePath)
+    return os.path.abspath(filePath)
+resetDir()
+
 
 debugMode = True
 
@@ -12,17 +26,25 @@ for row in rows:
 '''
 
 app = Flask(__name__)
-with sql.connect("login.db") as connection:
+with sql.connect("logins.db") as connection:
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)")
+    # Try to make email bot send recovery code to user email
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (userID INTEGER PRIMARY KEY, username TEXT, password TEXT, firstName TEXT, lastName TEXT, email TEXT)")
     connection.commit()
 
 @app.route("/")
 @app.route("/index")
 @app.route("/login")
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
 
+@app.route("/recovery")
+def forgot_password():
+    return render_template("forgot_password.html")
+
+@app.route("/registration")
+def register():
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.run(debug=debugMode,host='0.0.0.0')
