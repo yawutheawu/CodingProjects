ba416834e521c8c5b51716be310fa229e9ec91f7
yawutheawuAWAY <mayklprime@gmail.com>
2026-09-05 17:40:07 -0400

    This commit was pushed automatically to CodingProjects (Not the best idea)

---
+++ Java/javabasics/javabasics/javabasics.java
@@ -0,0 +1,40 @@
+package javabasics;
+
+public class javabasics {
+	public static void main(String[] args) {
+//		String x = "Hello World";
+//		System.out.println(x);
+//		int y  = 10;
+//		System.out.println(y);
+//		boolean z = true;
+//		System.out.println(z);
+//		double n = 10.75;
+//		System.out.println(n);
+//		char u = 'i';
+//		System.out.println(u);
+
+		// and -> &&
+		// or -> ||
+		// not -> !
+
+//		if (false) {
+//			System.out.println("Hello");
+//		} else if (true) {
+//			System.out.println("Bye");
+//		} else {
+//			System.out.println("No");
+//		}
+
+		char oper = '+';
+		int a = 10;
+		int b = 5;
+		if (oper == '+') {
+			System.out.println(a + b);
+		} else if (oper == '-') {
+			System.out.println(a - b);
+		} else {
+			System.out.println("Bad Input");
+		}
+
+	}
+}
