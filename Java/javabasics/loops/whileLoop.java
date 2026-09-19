2a1ecc0399bc0f91512d9290ec23f51eff5086f8
yawutheawuAWAY <mayklprime@gmail.com>
2026-09-19 17:35:21 -0400

    This commit was pushed automatically to CodingProjects (Not the best idea)

---
+++ Java/javabasics/loops/whileLoop.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class whileLoop {
+	public static void main(String[] args) {
+		
+		for (int i = 0;i < 7;i++) {
+			for (int n = 0; n < i; n++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		
+	}
+}
